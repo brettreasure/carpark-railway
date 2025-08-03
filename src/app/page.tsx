88b64abfe1eccda00149038a3eebbb06eafb0e62
brettreasure <treasure@freebeer.com.au>
2025-08-03 23:12:45 +0800

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import ImageCarousel from '@/components/ImageCarousel';
+
+export default function Home() {
+  return (
+    <div className="min-h-[calc(100vh-4rem)]">
+      <ImageCarousel />
+    </div>
+  );
+}
