d335c8a5ed69f542f5b3403d7cb5fe4e6c218b7f
brettreasure <treasure@freebeer.com.au>
2025-08-04 11:07:38 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,14 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
   // Temporary hardcoded values for Railway testing
   const supabaseUrl = 'https://btsozmbpopngjznmbbuc.supabase.co';
-  const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc
-  3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0c296bWJwb3BuZ2p6bm1iYnVjIiwicm9sZSI6
-  ImFub24iLCJpYXQiOjE3NTQxOTQ1MzYsImV4cCI6MjA2OTc3MDUzNn0.cXtxzH4WVx-
-  HoflcNI8Gslp1k4vIgcb9bXLB1cYgDlg';
-  const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ey
-  Jpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0c296bWJwb3BuZ2p6bm1iYnVjIiwicm9sZ
-  SI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDE5NDUzNiwiZXhwIjoyMDY5NzcwNTM2
-  fQ.SdtHg0vD2fDPMXj9one-qhWs-Y1GJ_fNlt-JRnnEInM';
+  const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0c296bWJwb3BuZ2p6bm1iYnVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxOTQ1MzYsImV4cCI6MjA2OTc3MDUzNn0.cXtxzH4WVx-HoflcNI8Gslp1k4vIgcb9bXLB1cYgDlg';
+  const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0c296bWJwb3BuZ2p6bm1iYnVjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDE5NDUzNiwiZXhwIjoyMDY5NzcwNTM2fQ.SdtHg0vD2fDPMXj9one-qhWs-Y1GJ_fNlt-JRnnEInM';
 
   export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
